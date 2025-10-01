37d01e4c6b7d41a1acb44b47fe7a6da0cb86f788
beomsoo <beomsoo.sn@gmail.com>
2025-10-01 11:06:44 -0700

    Remove not used files

--- packages/mujoco-core/dist/runtime/loadMujoco.d.ts
+++
@@ -1,3 +0,0 @@
-import type { CreateOptions, MujocoModule } from '../types.js';
-export declare function loadMujocoModule(options?: CreateOptions): Promise<MujocoModule>;
-//# sourceMappingURL=loadMujoco.d.ts.map
--- packages/mujoco-core/dist/worker/client.d.ts
+++
@@ -1,9 +0,0 @@
-import type { CreateOptions } from '../types.js';
-export interface MujocoWorkerHandle {
-    readonly worker: Worker;
-    readonly ready: Promise<void>;
-    terminate(): void;
-    call<T = unknown>(type: string, payload?: unknown): Promise<T>;
-}
-export declare function createMujocoWorker(options?: CreateOptions): MujocoWorkerHandle;
-//# sourceMappingURL=client.d.ts.map
